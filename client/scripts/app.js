3d67958e4994231c45aa0dbfc3779c9b2a37b281
Hack Reactor Students <mrwebbin@gmail.com>
2014-06-16 13:25:14 -0700

    Add function to add single message to DOM

--- client/scripts/app.js
+++ client/scripts/app.js
@@ -6,12 +6,39 @@ var app = {
     'text': 'test',
     'roomname': 'shawndrew room'
   },
+  clearMessages: function() {
+    $('#chats').html('');
+  },
+  addMessage: function(message) {
+    $('#chats').append('li').html(message.username + ': ' + message.text);
+  },
   init: function(){},
   send: function(message){
     $.ajax({
+      url: 'https://api.parse.com/1/classes/chatterbox',
       type: 'POST',
-      data: JSON.stringify(message)
+      data: JSON.stringify(message),
+      contentType: 'application/json',
+      success: function(data) {
+        console.log('Message with ID of ' + data.objectId + ' was sucessfully sent');
+      },
+      error: function(data) {
+        console.log('Message sending failed');
+      }
     });
-
+  },
+  fetch: function(){
+    $.ajax({
+      url: 'https://api.parse.com/1/classes/chatterbox',
+      type: 'GET',
+      success: function(data) {
+        app.clearMessages();
+        app.addMessage(data.results[0]);
+        //iterate through messages
+      }
+    });
   }
 };
+
+// app.send(app.message);
+// app.fetch();
