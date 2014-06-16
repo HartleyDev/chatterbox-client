4d11bf4b8b9c5464e45c0f5451cda224c0e2b843
Hack Reactor Students <mrwebbin@gmail.com>
2014-06-16 12:54:27 -0700

    Finish Init and Send Functionality

--- client/scripts/app.js
+++ client/scripts/app.js
@@ -1,2 +1,17 @@
 // YOUR CODE HERE:
 
+var app = {
+  message: {
+    'username': 'shawndrew',
+    'text': 'test',
+    'roomname': 'shawndrew room'
+  },
+  init: function(){},
+  send: function(message){
+    $.ajax({
+      type: 'POST',
+      data: JSON.stringify(message)
+    });
+
+  }
+};
