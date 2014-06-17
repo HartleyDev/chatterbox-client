8649f32139ce5856ac8c718a21440f698ea3679f
Hack Reactor Students <mrwebbin@gmail.com>
2014-06-16 19:28:42 -0700

    Add "Compose message" functionality. Note: change URI component decoding

--- client/scripts/app.js
+++ client/scripts/app.js
@@ -1,87 +1,104 @@
 // YOUR CODE HERE:
+$(document).ready(function(){
+  var app = {
 
-var app = {
-  message: {
-    'username': 'shawndrew',
-    'text': 'test',
-    'roomname': 'shawndrew room'
-  },
+    init: function(){
+      this.server = 'https://api.parse.com/1/classes/chatterbox';
+      setInterval(function(){
+        app.fetch();
+      }, 1000);
+    },
 
-  init: function(){
-    this.server = 'https://api.parse.com/1/classes/chatterbox';
-    setInterval(function(){
-      app.fetch();
-      console.log('refreshed');
-    }, 1000);
-  },
+    send: function(message){
+      $.ajax({
+        url: this.server,
+        type: 'POST',
+        data: JSON.stringify(message),
+        contentType: 'application/json',
+        success: function(data) {
+          console.log('Message with ID of ' + data.objectId + ' was sucessfully sent');
+        },
+        error: function(data) {
+          console.log('Message sending failed');
+        }
+      });
+    },
 
-  send: function(message){
-    $.ajax({
-      url: this.server,
-      type: 'POST',
-      data: JSON.stringify(message),
-      contentType: 'application/json',
-      success: function(data) {
-        console.log('Message with ID of ' + data.objectId + ' was sucessfully sent');
-      },
-      error: function(data) {
-        console.log('Message sending failed');
+    fetch: function(){
+      $.ajax({
+        url: this.server,
+        type: 'GET',
+        data: {order: '-createdAt', limit: 50},
+        success: function(data) {
+          app.clearMessages();
+          app.clearRooms();
+          app.addRooms(data.results);
+          app.addMessages(data.results);
+          //iterate through messages
+        }
+      });
+    },
+
+    sendMessage: function(){
+      var $text = $('.compose-chat').val();
+      var user = window.location.href.slice(window.location.href.indexOf('=') + 1).split('&')[0];
+      var room = window.location.href.slice(window.location.href.indexOf('=') + 1).split('&')[1].split('=')[1];
+      var message = {
+        'username': user,
+        'text': $text,
+        'roomname': room
+      };
+      app.send(message);
+    },
+
+    clearMessages: function() {
+      $('#chats').html('');
+    },
+
+    addMessages: function(messageArr){
+      for(var i = 0; i < messageArr.length; i++){
+        app.addMessage(messageArr[i]);
       }
-    });
-  },
+    },
 
-  fetch: function(){
-    $.ajax({
-      url: this.server,
-      type: 'GET',
-      success: function(data) {
-        app.clearMessages();
-        app.clearRooms();
-        app.addRooms(data.results);
-        app.addMessages(data.results);
+    addMessage: function(message) {
+      $('<li>').text( message.username + ': ' + message.text ).appendTo('#chats');
+    },
 
-        //iterate through messages
+    clearRooms: function(){
+      $('#roomSelect').html('');
+    },
+
+    addRoom: function(room){
+      $('<li>').text(room).appendTo('#roomSelect');
+    },
+
+    addRooms: function(messagesArr){
+      var rooms = _.uniq(_.pluck(messagesArr, 'roomname'));
+      var unNamed = false;
+      for(var i = 0; i < rooms.length; i++){
+        if(rooms[i]){
+          app.addRoom(rooms[i]);
+        }else {
+          unNamed = true;
+        }
       }
-    });
-  },
-
-  clearMessages: function() {
-    $('#chats').html('');
-  },
-
-  addMessages: function(messageArr){
-    for(var i = 0; i < messageArr.length; i++){
-      app.addMessage(messageArr[i]);
-    }
-  },
-
-  addMessage: function(message) {
-    $('<li>').text( message.username + ': ' + message.text ).appendTo('#chats');
-  },
-
-  clearRooms: function(){
-    $('#roomSelect').html('');
-  },
-
-  addRoom: function(room){
-    $('<li>').text(room).appendTo('#roomSelect');
-  },
-
-  addRooms: function(messagesArr){
-    var rooms = _.uniq(_.pluck(messagesArr, 'roomname'));
-    var unNamed = false;
-    for(var i = 0; i < rooms.length; i++){
-      if(rooms[i]){
-        app.addRoom(rooms[i]);
-      }else {
-        unNamed = true;
+      if(unNamed){
+        app.addRoom('default');
       }
     }
-    if(unNamed){
-      app.addRoom('default');
-    }
-  }
-};
+  };
 
-app.init();
-app.fetch();
+  $('.chat-button').on('click', function(){
+    if (!$('.compose-chat').val()) {
+      $('.compose-chat').val('We are too lazy to enter a message');
+    }
+    app.sendMessage();
+    $('.compose-chat').val('');
+  });
+
+  app.init();
+  app.fetch();
+
+});
+
